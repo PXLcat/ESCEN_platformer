8d610a90ee33f85cf01038c7304c844500788b50
PXLcat <pixellecat@gmail.com>
2022-03-22 23:27:43 +0100

    Ajout d'une pièce à collectionner

---
+++ Assets/CoinBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("+1 pièce");
+        //TODO Envoyer l'évènement
+
+        Destroy(this.gameObject); //Attention à bien préciser 
+    }
+}
