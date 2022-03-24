1ed5c7bd6855f31c2e9bad3bbf0d83f1e022baf4
PXLcat <pixellecat@gmail.com>
2022-03-24 12:06:52 +0100

    UI fonctionelle

---
+++ Assets/CoinsUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class CoinsUI : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _coinsValue;
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnCoinCollected += UpdateText;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        _coinsValue.text = GameManager.Instance.CoinsCollected.ToString();
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
